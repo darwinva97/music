4770b2141c7b808d11096d2e45de0ce776bf47a1
Darwin Vilcachagua Ayala <darwin97.va@gmail.com>
2023-11-27 19:42:00 -0500

    header responsive

--- src/app/(public)/footer.tsx
+++ src/app/(public)/footer.tsx
@@ -1,22 +1,53 @@
 import { Slider } from "@/components/ui/slider";
+import { Play, SkipBack, SkipForward } from "lucide-react";
 import Image from "next/image";
 
 export const Footer = () => {
   return (
-    <footer className="flex fixed bottom-0 left-0 w-full bg-[rgba(44,53,103,0.7)] p-4">
+    <footer className="flex fixed bottom-0 left-0 w-full bg-[rgba(44,53,103,0.7)] p-4 flex-col bg-blend-normal">
       <div className="flex items-center w-full gap-2">
         <span className="text-white">00:00</span>
-        <Slider rangeClassName="bg-red-500" trackClassName="bg-red-500" />
+        <Slider
+          rangeClassName="bg-red-500"
+          trackClassName="bg-red-500"
+          thumbClassName="cursor-pointer"
+        />
         <span className="text-white">00:00</span>
       </div>
       <div className="flex items-center w-full gap-2">
-        <Image src={"/next.svg"} width={50} height={50} alt="Song" />
-        <div className="flex flex-col items-start">
+        <Image
+          src={"/next.svg"}
+          width={50}
+          height={50}
+          alt="Song"
+          className="mr-1"
+        />
+        <div className="flex flex-col items-start text-white">
           <h3>Song</h3>
           <h4>Artist</h4>
         </div>
-        <div className="flex items-center">
-          Button
+        <div className="flex items-center justify-end flex-1 gap-4">
+          <SkipBack
+            color="#ffffff"
+            fill="white"
+            strokeWidth={1.75}
+            size={30}
+            cursor="pointer"
+          />
+          <Play
+            color="#ffffff"
+            fill="white"
+            strokeWidth={1.75}
+            size={30}
+            cursor="pointer"
+          />
+          <SkipForward
+            color="#ffffff"
+            fill="white"
+            strokeWidth={1.75}
+            size={30}
+            cursor="pointer"
+          />
         </div>
       </div>
     </footer>
--- src/app/(public)/header.tsx
+++ src/app/(public)/header.tsx
@@ -1,4 +1,14 @@
 import { Input } from "@/components/ui/input";
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import { Menu } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -7,17 +17,47 @@ export const Header = () => {
     <header className="flex items-center justify-between p-4 sticky top-0">
       <Image src="/vercel.svg" alt="Logo" width={120} height={27} />
       <Input className="w-auto" placeholder="Search" />
-      <ul className="flex items-center space-x-4">
-        <li>
+
+      <ul className="hidden md:flex p-2 gap-4">
+        <li className="ml-0">
           <Link href="/">Home</Link>
         </li>
-        <li>
+        <li className="ml-0">
           <Link href="/latest">Latest</Link>
         </li>
-        <li>
+        <li className="ml-0">
           <Link href="/albums">Albums</Link>
         </li>
       </ul>
+
+      <Sheet>
+        <SheetTrigger className="block md:hidden">
+          <Menu color="#000000" strokeWidth={1.75} />
+        </SheetTrigger>
+        <SheetContent>
+          <SheetHeader>
+            <SheetDescription>
+              <ul className="flex flex-col p-2 gap-2 pt-4 text-xl">
+                <li className="ml-0">
+                  <Link href="/">
+                    <SheetClose>Home</SheetClose>
+                  </Link>
+                </li>
+                <li className="ml-0">
+                  <Link href="/latest">
+                    <SheetClose>Latest</SheetClose>
+                  </Link>
+                </li>
+                <li className="ml-0">
+                  <Link href="/albums">
+                    <SheetClose>Albums</SheetClose>
+                  </Link>
+                </li>
+              </ul>
+            </SheetDescription>
+          </SheetHeader>
+        </SheetContent>
+      </Sheet>
     </header>
   );
 };
